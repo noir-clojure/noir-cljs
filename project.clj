bb26e0bbcc20869cbc2ee9e6089870052b81caa3
Chris Granger <ibdknox@gmail.com>
2011-08-07 13:43:00 -0700

    initial import
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject noir-cljs "0.1.0-SNAPSHOT"
+            :description "A noir utility to add CLJS compiling through middleware"
+            :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+                           [cljs-compiler-jar "0.1.0-SNAPSHOT"]
+                           [goog-jar "1.0.0"]])
---
+++ test/noir_cljs/test/core.clj
@@ -0,0 +1,6 @@
+(ns noir-cljs.test.core
+  (:use [noir-cljs.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
