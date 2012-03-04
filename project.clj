29e429bf602a9f144ff886e8ab9dee8e9555f8ec
Chris Granger <ibdknox@gmail.com>
2012-03-03 22:13:19 -0800

    move to alphas so that I can push to clojars
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "A noir utility to add CLJS compiling through middleware"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.2"]
-                 [fetch "0.1.0-SNAPSHOT"]
-                 [jayq "0.1.0-SNAPSHOT"]
-                 [crate "0.1.0-SNAPSHOT"]
+                 [fetch "0.1.0-alpha1"]
+                 [jayq "0.1.0-alpha1"]
+                 [crate "0.1.0-alpha1"]
                  [watchtower "0.1.0"]
                  [colorize "0.1.1"]
                  [org.clojure/clojurescript "0.0-971"]])
