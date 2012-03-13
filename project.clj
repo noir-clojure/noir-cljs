064e7e22d8d48df29eb0f487e7384bf7455a2a50
Chris Granger <ibdknox@gmail.com>
2012-03-13 12:27:48 -0700

    inc version with new crate
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject noir-cljs "0.2.3"
+(defproject noir-cljs "0.2.4"
   :description "A noir utility to add CLJS compiling through middleware"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.2"]
                  [fetch "0.1.0-alpha2"]
                  [jayq "0.1.0-alpha1"]
-                 [crate "0.1.0-alpha1"]
+                 [crate "0.1.0-alpha2"]
                  [watchtower "0.1.0"]
                  [colorize "0.1.1"]
                  [org.clojure/clojurescript "0.0-993"]])
