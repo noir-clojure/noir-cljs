641fd3cdffe18b489493ff7aaa571ea51ec7dff9
Chris Granger <ibdknox@gmail.com>
2012-03-20 10:12:27 -0700

    latest hiccup, 0.1.5
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject noir-cljs "0.2.5"
+(defproject noir-cljs "0.3.0"
   :description "A noir utility to add CLJS compiling through middleware"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.2.2"]
+                 [noir "1.3.0-beta2"]
                  [fetch "0.1.0-alpha2"]
                  [jayq "0.1.0-alpha1"]
-                 [crate "0.1.0-alpha2"]
-                 [watchtower "0.1.0"]
+                 [crate "0.1.0-alpha3"]
+                 [watchtower "0.1.1"]
                  [colorize "0.1.1"]
                  [org.clojure/clojurescript "0.0-993"]])
