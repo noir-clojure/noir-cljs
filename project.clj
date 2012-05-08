cc6b717cf77b053bddc2ee6a3d9560f255a33514
Chris Granger <ibdknox@gmail.com>
2012-05-07 23:26:25 -0700

    move to the latest crate
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject noir-cljs "0.3.0"
+(defproject noir-cljs "0.3.2"
   :description "A noir utility to add CLJS compiling through middleware"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta2"]
                  [fetch "0.1.0-alpha2"]
                  [jayq "0.1.0-alpha1"]
-                 [crate "0.1.0-alpha3"]
+                 [crate "0.2.0-alpha2"]
                  [watchtower "0.1.1"]
                  [colorize "0.1.1"]
-                 [org.clojure/clojurescript "0.0-993"]])
+                 [org.clojure/clojurescript "0.0-1011"]])
--- src/noir/cljs/client/watcher.cljs
+++ src/noir/cljs/client/watcher.cljs
@@ -3,8 +3,8 @@
             [crate.core :as crate]
             [cljs.reader :as reader])
   (:use [jayq.core :only [$ append delegate data add-class remove-class find]]
-        [crate.tags :only [link-to]])
-  (:use-macros [crate.macros :only [defpartial]]))
+        [crate.element :only [link-to]])
+  (:use-macros [crate.def-macros :only [defpartial]]))
 
 (defn wait [ms func]
   (js* "setTimeout(~{func}, ~{ms})"))
